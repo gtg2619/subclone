0e657a52e395172945d02cf476fb96f74115756b
randomErr <2497404097@qq.com>
2023-12-11 18:26:29 +0800

    [no messgae]

--- cli.js
+++ cli.js
@@ -6,6 +6,6 @@ var subclone = require('./index');
 (async()=>{
 await subclone(argv.url || argv.u || argv["_"][0] || process.exit(1), {
     "token": argv.token || argv.t || process.env.github_access_token || "",
-    "customPath": argv.p || argv.customPath || argv.path || process.env.customPath || undefined
+    "customPath": argv.p || argv.customPath || argv.path || argv.o || argv.output || process.env.customPath || undefined
 }).then(folder_name=>folder_name);
 })();
